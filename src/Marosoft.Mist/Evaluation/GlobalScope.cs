7f11327d36c08592af7ba84f3fdc04dbdf0ffbb3
tormaroe <torbjorn.maro@gmail.com>
2011-08-13 12:53:32 +0200

    Cleaned up GlobalScope

--- src/Marosoft.Mist/Evaluation/GlobalScope.cs
+++ src/Marosoft.Mist/Evaluation/GlobalScope.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Marosoft.Mist.Parsing;
-using Marosoft.Mist.Lexing;
 using System.Reflection;
 
 namespace Marosoft.Mist.Evaluation
@@ -16,12 +12,10 @@ namespace Marosoft.Mist.Evaluation
             AddBinding("true", new SpecialSymbolExpression("true", true));
             AddBinding("false", new SpecialSymbolExpression("false", false));
             
-            /**
-             *      Adding the "built in" functions implemented in C#
-             **/
-
-            Array.ForEach(Assembly.GetCallingAssembly().GetTypes(), AddGlobalFunction);
-
+            // Adding the "built in" functions implemented in C#
+            Array.ForEach(
+                Assembly.GetCallingAssembly().GetTypes(), 
+                AddGlobalFunction);
         }
 
         private void AddGlobalFunction(Type t)
@@ -32,18 +26,5 @@ namespace Marosoft.Mist.Evaluation
                 AddBinding((BuiltInFunction)Activator.CreateInstance(t, this));
         }
 
-        public override Expression Resolve(string symbol)
-        {
-            try
-            {
-                return base.Resolve(symbol);
-            }
-            catch (SymbolResolveException)
-            {
-                var t = Type.GetType(symbol); // WORK IN PROGRESS!!!!
-
-                throw;
-            }
-        }
     }
 }
