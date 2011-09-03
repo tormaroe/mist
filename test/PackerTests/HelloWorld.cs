ccb52c31ef6b84edc0cbb38da8aa86f4a8930495
tormaroe <torbjorn.maro@gmail.com>
2011-09-03 20:17:21 +0200

    (Refactored mistpacker test)

--- test/PackerTests/HelloWorld.cs
+++ test/PackerTests/HelloWorld.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System.IO;
 using System.Threading;
 using System.Diagnostics;
@@ -12,27 +8,12 @@ namespace test.PackerTests
     [TestFixture]
     public class HelloWorld
     {
+        /// <summary>
+        /// Compilation and execution of mist program is done
+        /// with this folder as current folder.
+        /// It's created in Setup and completely deleted in Teardown.
+        /// </summary>
         const string testfolder = "testfolder";
-
-        [SetUp]
-        public void Setup()
-        {
-            Directory.CreateDirectory(testfolder);
-            File.Copy("Marosoft.Mist.dll", Path.Combine(testfolder, "Marosoft.Mist.dll"));
-            File.Copy("mistpacker.exe", Path.Combine(testfolder, "mistpacker.exe"));
-            Directory.SetCurrentDirectory(testfolder);
-        }
-
-        [TearDown]
-        public void Teardown()
-        {
-            Directory.SetCurrentDirectory("..");
-            File.Delete(Path.Combine(testfolder, "Marosoft.Mist.dll"));
-            File.Delete(Path.Combine(testfolder, "mistpacker.exe"));
-            File.Delete(Path.Combine(testfolder, "out.exe"));
-            Thread.Sleep(500);
-            Directory.Delete(testfolder);
-        }
 
         [Test]
         public void Test()
@@ -68,5 +49,28 @@ namespace test.PackerTests
             p2.ExitCode.ShouldEqual(0);
             p2.StandardOutput.ReadToEnd().ShouldEqual("hello, world");
         }
+
+        [SetUp]
+        public void Setup()
+        {
+            Directory.CreateDirectory(testfolder);
+            File.Copy("Marosoft.Mist.dll", Path.Combine(testfolder, "Marosoft.Mist.dll"));
+            File.Copy("mistpacker.exe", Path.Combine(testfolder, "mistpacker.exe"));
+
+            Directory.SetCurrentDirectory(testfolder);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Directory.SetCurrentDirectory("..");
+
+            File.Delete(Path.Combine(testfolder, "Marosoft.Mist.dll"));
+            File.Delete(Path.Combine(testfolder, "mistpacker.exe"));
+            File.Delete(Path.Combine(testfolder, "out.exe"));
+            Thread.Sleep(500);
+            Directory.Delete(testfolder);
+        }
+
     }
 }
